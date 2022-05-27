ea66812367f938b3504938eeb27829fefe3bff9d
AlpinistV <spasovecvadim@gmail.com>
2022-05-27 12:28:22 +0300

    1. метод getUser сделай не через запрос, а методом entityManager 2. в методе updateUser нужно использовать соответствующий метод entityManager

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,42 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUsers() {
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u", User.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUser(long id) {
+        return entityManager.createQuery(("from User where id=" + id), User.class).getSingleResult();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        User oldUser = entityManager.find(User.class, id);
+        entityManager.remove(oldUser);
+    }
+}
